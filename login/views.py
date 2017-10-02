62b6105e15e2a33dde4e448cab25395687d63887
Ankit-22 <ankitmani.t@somaiya.edu>
2017-10-02 23:53:30 +0530

    Fix all Bugs and Homepage Schedule

--- login/views.py
+++ login/views.py
@@ -7,14 +7,14 @@ import bcrypt
 # Create your views here.
 def schedule(request, waste_data):
 	schedules = Schedule.objects.all()
-	return render(request, 'login/schedule.html', {"team_name": request.session['username'], "team_repo": request.session['github_repo'], "list": schedules})
+	return render(request, 'login/schedule.html', {"list": schedules, "team_name": request.session['username'], "team_repo": request.session['github_repo']})
 
 def rules(request, waste_data):
 	return render(request, 'login/rules-and-guidelines.html', {"team_name": request.session['username'], "team_repo": request.session['github_repo']})
 
 def login(request):
 	if request.session.get('logged', False) == True:
-		return redirect('/home/'+str(request.session['id']))
+		return redirect('/home/'+str(request.session['id'])+'/')
 
 	if request.method == 'POST':
 		username = request.POST.get('username')
@@ -27,21 +27,21 @@ def login(request):
 			if password == hashed_password:
 				request.session['message'] = ''
 				request.session['logged'] = True
-				request.session['id'] = id
+				request.session['id'] = str(id)
 				request.session['username'] = query.username
 				if query.github_repo:
 					request.session['github_repo'] = query.github_repo
 				else:
-					request.session['github_repo'] = "../"+str(id)+"#"
+					request.session['github_repo'] = "../"+str(id)+"/#"
 				request.session['wifi_user_1'] = query.wifi_username_1
 				request.session['wifi_user_2'] = query.wifi_username_2
 				request.session['wifi_pass_1'] = query.wifi_password_1
 				request.session['wifi_pass_2'] = query.wifi_password_2
-				return redirect('/home/'+id)
+				return redirect('/home/'+str(id)+'/')
 			request.session['message'] = "Invalid password or username"
 		except:
 			request.session['message'] = "Invalid password or username"
-			return redirect('/login')
+			return redirect('/login/')
 	message = ''
 	if('message' in request.session):
 		message= request.session['message']
@@ -51,10 +51,11 @@ def login(request):
 
 
 def home(request, id):
-	if(request.session['id']):
-		return render(request,'login/landing_page.html',{"title": "Home", "id": id, "team_name": request.session['username'], "team_repo": request.session['github_repo'], "username1": request.session['wifi_user_1'], "username2": request.session['wifi_user_2'], "password1": request.session['wifi_pass_1'], "password2": request.session['wifi_pass_2']})
+	if(request.session['id'] == id):
+		schedules = Schedule.objects.all()
+		return render(request,'login/landing_page.html',{"list": schedules, "title": "Home", "id": id, "team_name": request.session['username'], "team_repo": request.session['github_repo'], "username1": request.session['wifi_user_1'], "username2": request.session['wifi_user_2'], "password1": request.session['wifi_pass_1'], "password2": request.session['wifi_pass_2']})
 	else:
-		return redirect('/login')
+		return redirect('/login/')
 
 
 def logout(request):
